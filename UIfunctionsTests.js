4ccdee620e318cea4d2c2b72af76072acb2b65e5
Catherine Hilton <126924004+cat-thebeesknees@users.noreply.github.com>
2023-06-15 12:49:59 -0400

    Add files via upload
    test functions

---
+++ UIfunctionsTests.js
@@ -0,0 +1,35 @@
+/// <reference path = "C:\Users\cathe\Coursework\Puppy-Bowl\PUPPYBOWL-cat\script.js"/>
+
+// xdescribe('all-players-container', function () {
+//     it('fetchAllPlayers function run with no errors', function () {
+//         fetchAllPlayers();
+//     });
+    
+// });
+
+describe('Testing the functionality, this is the checklist', ()=>{
+    it('should show all players', ()=>{
+      //...
+    })
+    it('should delete a player', ()=>{
+      //...
+    })
+    it('should show a single players details', ()=>{
+      //...
+    })
+    it('should create a add new player form', ()=>{
+    //...
+    })
+    it('should create a team form', ()=>{
+    //...
+    })
+  })
+
+//   // validate the form
+// 	let nameValid = hasValue(form.elements["name"], NAME_REQUIRED);
+// 	let emailValid = validateEmail(form.elements["email"], EMAIL_REQUIRED, EMAIL_INVALID);
+// 	// if valid, submit the form.
+// 	if (nameValid && emailValid) {
+// 		alert("Demo only. No form was posted.");
+// 	}
+// });
